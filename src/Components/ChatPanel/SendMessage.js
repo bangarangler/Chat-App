1b2cb239dec3d5031665299819e0482c2717e9a4
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-04 16:27:29 -0400

    Add new msg to data base
    with timestamp from db

--- src/Components/ChatPanel/SendMessage.js
+++ src/Components/ChatPanel/SendMessage.js
@@ -1,22 +1,46 @@
-import React from 'react'
-import './SendMessage.css'
+import React, {useContext, useState} from 'react';
+import UserContext from '../../context/UserContext.js';
+import CurrentRoomContext from '../../context/CurrentRoomContext.js';
+import './SendMessage.css';
+import firebase from '../../logic/firebase.js'
 
-const SendMessage = ({ messagesRefFirebase }) => {
+const SendMessage = ({messagesRefFirebase}) => {
+  const {user} = useContext(UserContext);
+  const {currentRoom} = useContext(CurrentRoomContext);
+  const [messageText, setMessageText] = useState('');
+  const createMessage = messageId => ({
+    id: messageId,
+    text: messageText,
+    roomId: currentRoom.id,
+    timestamp: firebase.database.ServerValue.TIMESTAMP,
+    user,
+  });
+
+  const messageId = messagesRefFirebase.push().key;
+  const newMessage = createMessage(messageId);
+
   return (
-    <div className='SendMessage'>
-      <input placeholder="insert message ..." />
+    <div className="SendMessage">
+      <input
+        onChange={e => setMessageText(e.target.value)}
+        placeholder="insert message ..."
+        value={messageText}
+      />
       <button
         onClick={() =>
           messagesRefFirebase
-            .child('id2')
-            .set({text: 'some text 2'})
-            .then(msg => console.log(`set success : ${msg}`))
+            .child(messageId)
+            .set(newMessage)
+            .then(msg => {
+              console.log(`set success`);
+              setMessageText('');
+            })
             .catch(err => console.log(`set error: ${err}`))
         }>
         send message
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default SendMessage;
