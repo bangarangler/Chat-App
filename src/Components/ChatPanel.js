c747451abb464b80ebb478ec8145e8f0c7871dac
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-04 13:36:31 -0400

    Added input for messages

--- src/Components/ChatPanel.js
+++ src/Components/ChatPanel.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import './ChatPanel.css'
+import './ChatPanel.css';
 
 function ChatPanel() {
   return (
-    <div className='ChatPanel'>
-    <div>Room 1</div>
-    <div style={{flexGrow: 1}}>Messages</div>
-    <div>Send Messages</div>
+    <div className="ChatPanel">
+      <div>Room 1</div>
+      <div style={{flexGrow: 1}}>
+        <ul>
+          <li>message 1</li>
+          <li>message 2</li>
+          <li>message 3</li>
+          <li>message 4</li>
+        </ul>
+      </div>
+      <div style={{display: 'flex'}}>
+        <input style={{flexGrow: 1}} placeholder="insert message ..." />
+            <button>send message</button>
+      </div>
     </div>
   );
 }
-
 
 export default ChatPanel;
