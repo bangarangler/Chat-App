a1aade40ca24ac6a36aa23fd658541b6b11a1770
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-04 17:55:26 -0400

    Working on dynamically adding Rooms

--- src/Components/SidePanel/Rooms.js
+++ src/Components/SidePanel/Rooms.js
@@ -1,12 +1,32 @@
-import React, { useContext } from 'react'
-import CurrentRoomContext from '../../context/CurrentRoomContext.js'
+import React, {useContext} from 'react';
+import CurrentRoomContext from '../../context/CurrentRoomContext.js';
+import firebase from '../../logic/firebase.js';
 
 const Rooms = () => {
-  const { setCurrentRoom } = useContext(CurrentRoomContext)
+  const {setCurrentRoom} = useContext(CurrentRoomContext);
+  const roomsRefFirebase = firebase.database().ref('rooms');
+
+  const addRoom = () => {
+    console.log('addRoom');
+    const roomId = roomsRefFirebase.push().key;
+    const newRoom = {
+      id: roomId,
+      name: 'room2', //TODO: replace with data from modal
+      description: 'desc2', // TODO: replace with data from modal
+    };
+
+    roomsRefFirebase
+      .child(roomId)
+      .set(newRoom)
+      .then(room => console.log(`success : ${room}`))
+      .catch(err => console.log(`error : ${err}`));
+  };
   return (
     <div>
-    <ul>
-      <li onClick={() => setCurrentRoom({
+      <p>rooms (...)</p>
+      <button onClick={addRoom}>Add Room</button>
+      <ul>
+        {/*<li onClick={() => setCurrentRoom({
         id: 'idroom1',
         name: 'room1',
         description: 'desc1'
@@ -15,10 +35,10 @@ const Rooms = () => {
       id: 'idroom2',
       name: 'room2',
       description: 'desc2'
-    })}>Room 2</li>
-    </ul>
+    })}>Room 2</li>*/}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Rooms;
