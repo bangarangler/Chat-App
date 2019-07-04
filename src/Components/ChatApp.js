ef2e8565a521135f9d507b49035c883965733e15
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-04 15:28:04 -0400

    Setting up Context for state management

--- src/Components/ChatApp.js
+++ src/Components/ChatApp.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import SidePanel from './SidePanel/SidePanel.js'
-import ChatPanel from './ChatPanel/ChatPanel.js'
-import './ChatApp.css'
+import React, {useState} from 'react';
+import SidePanel from './SidePanel/SidePanel.js';
+import ChatPanel from './ChatPanel/ChatPanel.js';
+import './ChatApp.css';
+import CurrentRoomContext from '../context/UserContext.js';
+import UserContext from '../context/UserContext.js';
 
 const ChatApp = () => {
+  const [user, setUser] = useState(null);
+  const [currentRoom, setCurrentRoom] = useState(null);
   return (
-    <div className='ChatApp'>
-      <SidePanel />
-      <ChatPanel />
+    <div className="ChatApp">
+      <UserContext.Provider value={{user, setUser}}>
+        <CurrentRoomContext.Provider value={{currentRoom, setCurrentRoom}}>
+          <SidePanel />
+          <ChatPanel />
+        </CurrentRoomContext.Provider>
+      </UserContext.Provider>
     </div>
   );
-}
+};
 
 export default ChatApp;
---
+++ src/context/CurrentRoomContext.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const currentRoomContext = React.createContext()
+export default currentRoomContext;
