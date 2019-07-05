1c654aedfb73b3b9770f79a1ba6ca9f70a5a0889
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-05 13:57:57 -0400

    Added in Login UI and hooked it up to firebase...
    tested and can successfully login with a created user

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import {appName, appIconName} from '../../logic/Constants.js';
+import {Segment, Header, Icon, Button, Form} from 'semantic-ui-react';
+import UserContext from '../../context/UserContext.js'
+import firebase from '../../logic/firebase.js'
+import './Login.css'
 
-const Login = () => {
+const Login = ({ history }) => {
+  const { setUser } = useContext(UserContext)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const login = e => {
+    e.preventDefault()
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then(loggedinUser => {
+      console.log(loggedinUser.user)
+      setUser({displayName: loggedinUser.user.displayName,
+      uid: loggedinUser.user.uid,
+        photoURL: loggedinUser.user.photoURL
+      })
+      history.push("/")
+    }).catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <div>
-    login
+    <div className="Login">
+     <Segment stacked>
+        <Header color="black" as="h2">
+          <Icon name={appIconName} /> Login to {appName}
+        </Header>
+        <Form onSubmit={login}>
+          <Form.Input
+            icon="mail"
+            value={email}
+            iconPosition="left"
+            placeholder="E-mail address"
+            type="email"
+            onChange={e => setEmail(e.target.value)}
+          />
+          <Form.Input
+            icon="lock"
+            value={password}
+            iconPosition="left"
+            placeholder="Password"
+            type="password"
+            onChange={e => setPassword(e.target.value)}
+          />
+          <Button size="large" fluid color="black" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Segment>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
