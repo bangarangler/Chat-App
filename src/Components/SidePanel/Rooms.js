e09b2a3b3d498e780c8734a97c15e3a06f7d0cbd
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-05 15:47:03 -0400

    Added in Spinner

--- src/Components/SidePanel/Rooms.js
+++ src/Components/SidePanel/Rooms.js
@@ -3,10 +3,12 @@ import CurrentRoomContext from '../../context/CurrentRoomContext.js';
 import firebase from '../../logic/firebase.js';
 import AddRoomModal from './AddRoomModal.js'
 import './Rooms.css'
+import Spinner from '../Common/Spinner.js'
 
 const Rooms = () => {
   const {setCurrentRoom} = useContext(CurrentRoomContext);
   const [rooms, setRooms] = useState([])
+  const [roomsLoaded, setRoomsLoaded] = useState(false)
   const roomsRefFirebase = firebase.database().ref('rooms');
 
   const refTo_roomsVariable = useRef()
@@ -18,6 +20,7 @@ const Rooms = () => {
     roomsRefFirebase.on('child_added', snap => {
       let newRooms = [...refTo_roomsVariable.current, snap.val()]
       setRooms(newRooms)
+      setRoomsLoaded(true)
     })
   }
 
@@ -32,11 +35,11 @@ const Rooms = () => {
     return () => removeRoomsListener()
   }, [])
 
-  const roomsElements = (
+  const roomsElements = roomsLoaded ? (
   <ul>
       {rooms.map((it, index) => <li onClick={() => setCurrentRoom(rooms[index])} key={index}>{it.name}</li>)}
   </ul>
-  )
+  ) : <Spinner />
 
   return (
     <div className="Rooms">
