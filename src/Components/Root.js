46386ae252d68cbe1fde77ecf1e6c3f642af6d19
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-05 14:18:35 -0400

    Added onAuthStateChanged to check if a user is...
    logged in already when navigating to /. if so they will login else
    redirect to /Login

--- src/Components/Root.js
+++ src/Components/Root.js
@@ -1,12 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import ChatApp from './ChatApp.js';
 import Login from '../Components/Auth/Login.js';
 import Register from '../Components/Auth/Register.js';
 import UserContext from '../context/UserContext.js';
+import firebase from '../logic/firebase.js'
 
-const Root = () => {
-  const [user, setUser] = useState(null);
+const Root = ({ history }) => {
+  const [user, setUser] = useState({displayName: "", uid: "", photoURL: ""});
+
+  useEffect(() => {
+    console.log(`mount`)
+  firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+      console.log(`already logged in -> nav to /`)
+      const { displayName, uid, photoURL } = user;
+      setUser({ displayName, uid, photoURL })
+      history.push("/")
+    } else {
+        console.log(`not logged in -> navigate to /Login`)
+        history.push("/Login")
+    }
+  })
+  },[]);
 
   return (
     <UserContext.Provider value={{user, setUser}}>
