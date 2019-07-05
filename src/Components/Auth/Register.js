38c33792734a818cb09a49ab8ccdab5823b0bbfc
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-05 13:41:28 -0400

    Wired up firebase to accept ...
    photoURL, and displayName along with email and password for registration

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,11 +1,11 @@
 import React, {useContext, useState} from 'react';
 import UserContext from '../../context/UserContext.js';
 import firebase from '../../logic/firebase.js';
-import {Header, Icon, Form, Button} from 'semantic-ui-react';
+import {Header, Icon, Form, Button, Segment} from 'semantic-ui-react';
 import {appName, appIconName} from '../../logic/Constants.js';
 import './Register.css';
 
-const Register = () => {
+const Register = ({ history }) => {
   const {setUser} = useContext(UserContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -19,13 +19,18 @@ const Register = () => {
       .createUserWithEmailAndPassword(email, password)
       .then(createdUser => {
         console.log(`createdUser : ${createdUser}`);
-        // TODO: add user to firebase
+        createdUser.user.updateProfile({displayName, photoURL}).then(() => {
+          console.log(createdUser.user)
+          setUser({displayName, photoURL, uid: createdUser.user.uid})
+          history.push("/")
+        })
       })
       .catch(err => console.log(`error : ${err}`));
   };
 
   return (
     <div className="Register">
+    <Segment stacked>
       <Header as="h2">
         <Icon name={appIconName} />
         Register to {appName}
@@ -37,7 +42,7 @@ const Register = () => {
           iconPosition="left"
           placeholder="Display Name"
           type="text"
-          onChange={e => e.target.value}
+          onChange={e => setDisplayName(e.target.value)}
         />
         <Form.Input
           icon="mail"
@@ -67,6 +72,7 @@ const Register = () => {
           Register
         </Button>
       </Form>
+    </Segment>
     </div>
   );
 };
